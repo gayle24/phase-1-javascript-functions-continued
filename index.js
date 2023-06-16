6c6d6ef2ef41f19d92c871614afae9ec2c8b19ef
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-16 19:40:15 +0300

    js functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+    
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+   return ("This Monday, I will " + activity + ".");
+    
+}    
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(str = "*") {
+    return function(adj = "special") {
+        return `You are ${str}${adj}${str}!`
+    }
+    
+}
+
